6e08eb0e330c8324051037dda4782f1d4e1260da
Davhdez23 <123275807+Davhdez23@users.noreply.github.com>
2023-04-22 19:43:46 -0600

    ExamenParcial3

---
+++ flutter_parcial3/lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+  
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+ const List<Widget> _widgetOptions = <Widget>[
+    MyApp(),
+    //Ejemplo(),
+  ];
+class _MyAppState extends State<MyApp> {
+  List _users = [];
+
+  Future<void> _fetchUsers() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _users = jsonDecode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Users'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+      
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Datos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_add),
+            label: 'Integrante',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_applications),
+            label: 'Configuracion',
+          ),
+        ],
+      ),
+        body: _users.isEmpty
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: _users.length,
+                itemBuilder: (context, index) {
+                  final user = _users[index];
+                  return Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            user['name'],
+                            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Email: ${user['email']}',
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Phone: ${user['phone']}',
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Website: ${user['website']}',
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+      ),
+    );
+  }
+}
+/*
+class Ejemplo extends StatelessWidget {
+  const Ejemplo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('David Ernesto Hernandez Gallardo 25-1642-2019'),);
+  }
+}*/
+
+
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Users'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+      
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Datos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            label: 'Integrante',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_applications),
+            label: 'Configuracion',
+          ),
+        ],
+      ),
+        body: Container()
+      ),
+    );
+  }
+}
